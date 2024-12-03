1215f42e2c8cf7eb08feaf0de98d1bec80aebe0e
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-03 17:15:45 +0300

    recreation12

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// getting places from APIs
+
 function loadPlaces(position) {
     const params = {
-        radius: 1000,    // search places not farther than this value (in meters)
+        radius: 500,    
         clientId: 'TP325WB3HL43TLO3IY5XS52EFS2GLXUKD1FWYBTHOTCD3MVI',
         clientSecret: 'YEDHWPKLGAGREQJ5CEJQ3FXX50JNGUE1SALTNABWJTXBUVAQ',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
-    // CORS Proxy to avoid CORS problems
+   
     const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
-    // Foursquare API (limit param: number of maximum places to fetch)
     const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
         &ll=${position.latitude},${position.longitude}
         &radius=${params.radius}
@@ -34,17 +33,14 @@ function loadPlaces(position) {
 window.onload = () => {
     const scene = document.querySelector('a-scene');
 
-    // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
 
-        // than use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
             .then((places) => {
                 places.forEach((place) => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
 
-                    // add place name
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
