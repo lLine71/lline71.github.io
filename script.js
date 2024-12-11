ba18b996b9581a2fd594da058290baad50b90da5
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-11 17:48:30 +0300

    OSM transfer15test

--- script.js
+++ script.js
@@ -27,36 +27,38 @@ window.onload = () => {
 	//var positioncords;
 	//const positioncords = 	{latitude: 55.869743, longitude: 37.600729};
 	
-	const positioncords = navigator.geolocation.getCurrentPosition(succes);
-	gcam.addEventListener("gps-camera-update-position", e => {
-		loadPlaces(positioncords)
-			.then((places) => {
-				places.elements.forEach((place) => {
-					console.log(place);
-					//const latitud = place.lat;
-					//const longitude = place.lon; //мб не const
-					//const name = place.tags.name;
-					alert(name);
-					const placeText = document.createElement('a-link');
-					placeText.setAttribute('gps-new-entity-place', {
-						latitude: place.lat + 0.001,
-						longitude: place.lon
-					});
-					
-					placeText.setAttribute('title', place.tags.name);
-					placeText.setAttribute('scale', '25 25 25');
-								
-					placeText.addEventListener('loaded', () => { //мб убрать
-						window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
-					});
+	//const positioncords = navigator.geolocation.getCurrentPosition(succes);
+	return navigator.geolocation.getCurrentPosition(function (position) { //maybe put down
+		gcam.addEventListener("gps-camera-update-position", e => {
+			loadPlaces(position.cords)
+				.then((places) => {
+					places.elements.forEach((place) => {
+						console.log(place);
+						//const latitud = place.lat;
+						//const longitude = place.lon; //мб не const
+						//const name = place.tags.name;
+						alert(name);
+						const placeText = document.createElement('a-link');
+						placeText.setAttribute('gps-new-entity-place', {
+							latitude: place.lat + 0.001,
+							longitude: place.lon
+						});
+						
+						placeText.setAttribute('title', place.tags.name);
+						placeText.setAttribute('scale', '25 25 25');
+									
+						placeText.addEventListener('loaded', () => { //мб убрать
+							window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
+						});
 
-					document.querySelector("a-scene").appendChild(placeText);
+						document.querySelector("a-scene").appendChild(placeText);
+				});
+								
 			});
-							
 		});
-		
-	console.log('hihihi')
+	
 	});	
+	console.log('hihihi')
 };
    // return navigator.geolocation.getCurrentPosition(function (position) {
 
